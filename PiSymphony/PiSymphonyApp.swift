98bf6449e34cf027c6658bf9fdde1b30407c81af
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-11-28 16:29:55 -0600

    Initial Commit

---
+++ PiSymphony/PiSymphonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiSymphonyApp.swift
+//  PiSymphony
+//
+//  Created by Ry Wiese on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct PiSymphonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

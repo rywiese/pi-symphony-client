8450d958b9b941f5175931ccf7f944b5c830b41d
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2021-07-29 18:13:39 -0500

    refactor httprequester

--- Http/HttpRequester.swift
+++ Http/HttpRequester.swift
@@ -34,25 +34,30 @@ class HttpRequester {
         body: Data? = nil
     ) -> Data {
         let url = URL(string: "http://\(hostname):\(port)\(endpoint)")
-        var request = URLRequest(url: url!)
-        request.httpMethod = verb
-        request.httpBody = body
-        request.setValue(
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = verb
+        urlRequest.httpBody = body
+        urlRequest.setValue(
             "application/json",
             forHTTPHeaderField: "Content-Type"
         )
-        var urlResponse: HTTPURLResponse? = nil
-        var responseBody: Data? = nil
+        return blockingSend(
+            urlRequest: urlRequest
+        )
+    }
+    
+    func blockingSend(
+        urlRequest: URLRequest
+    ) -> Data {
+        var urlResponseBody: Data? = nil
         let semaphore = DispatchSemaphore.init(value: 0)
-        let task = session.dataTask(with: request) { data, response, error in
-            if error != nil {print(error)}
-            urlResponse = response as! HTTPURLResponse
-            if urlResponse?.statusCode != nil {print("status code: \(urlResponse?.statusCode)")}
-            responseBody = data!
-            semaphore.signal()
-        }.resume()
+        session
+            .dataTask(with: urlRequest) { data, response, error in
+                urlResponseBody = data
+                semaphore.signal()
+            }
+            .resume()
         semaphore.wait()
-        print(urlResponse)
-        return responseBody!
+        return urlResponseBody!
     }
 }
